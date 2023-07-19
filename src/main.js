f1c5bccbd0f4d880f088b3a951f3bfab2df21442
Constanza Latorre <constanzalatorrecid@gmail.com>
2023-07-19 14:00:09 -0400

    progreso

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 import data from '/data/tarot/tarot.js';
-const contenedor= document.getElementById("root");
-console.log (contenedor)
+const contenedor = document.getElementById("root");
+console.log(data)
+const cards = data.cards
+console.log(cards)
+cards.forEach(element => {
+
+    contenedor.innerHTML+=`
+    <div>
+        <img src=${element.img} alt=${element.name}>
+        <p>${element.name}</p>  
+    </div>` ;   
+
+});
